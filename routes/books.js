f2c77463d540bc2e76730c952e8046806614fa5c
Gabriel Cornejo <gcornejo441@gmail.com>
2021-01-15 00:22:10 -0800

    Form ata -> Local MongoDB

--- routes/books.js
+++ routes/books.js
@@ -1,29 +1,43 @@
-const express = require('express');
+const express = require("express");
 const bookRouter = express.Router();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const MongoClient = require("mongodb").MongoClient;
+const test = require("assert");
+
+const url = "mongodb://localhost:27017";
+
+const dbName = "chapters";
 
 bookRouter.use(bodyParser.urlencoded({ extended: false }));
 bookRouter.use(bodyParser.json());
-
-// Where we will keep books
-let books = [];
-
 bookRouter.use(cors());
 
-bookRouter.route('/')
-.get((req, res, next) => {
-    res.sendFile(path.join(__dirname + '/new-book.html'));
-})
-.post((req, res, next) => {
-    const book = req.body;
+MongoClient.connect(url, (err, client) => {
+  // Admin database for the operation
+  const coll = client.db(dbName).collection("books");
 
-    console.log(book);
-    books.push(book)
-    
-    res.send('Book is added to the database');
-})
+  bookRouter.route("/")
+    .get((req, res, next) => {
+      res.sendFile(path.join(__dirname + "/new-book.html"));
+    })
+    .post((req, res, next) => {
+    // Document
+      const book = req.body;
+      console.log(book);
+
+      coll.insert(book, {w:1}, (err, result) => {
+          test.equal(null, err);
+
+          dbName.listCollections().toArray((err, items) => {
+              test.equal(null, err);
+              client.close();
+          });
+      });
+      
+      res.send("Book is added to the database");
+    });
+});
 
 module.exports = bookRouter;
