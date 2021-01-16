b956cc094c2082a4bf97ad9e92481783ad872b3a
Gabriel Cornejo <gcornejo441@gmail.com>
2021-01-15 23:31:25 -0800

    Setting Book Routes and HTML Files

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
-const test = require("assert");
+const assert = require("assert");
 
 const url = "mongodb://localhost:27017";
 
@@ -12,32 +12,55 @@ const dbName = "chapters";
 
 bookRouter.use(bodyParser.urlencoded({ extended: false }));
 bookRouter.use(bodyParser.json());
-bookRouter.use(cors());
 
 MongoClient.connect(url, (err, client) => {
   // Admin database for the operation
   const coll = client.db(dbName).collection("books");
 
-  bookRouter.route("/")
+  bookRouter
+    .route("/")
     .get((req, res, next) => {
       res.sendFile(path.join(__dirname + "/new-book.html"));
     })
     .post((req, res, next) => {
-    // Document
+      // Document
       const book = req.body;
       console.log(book);
 
-      coll.insert(book, {w:1}, (err, result) => {
-          test.equal(null, err);
+      coll.insert(book, { w: 1 }, (err, result) => {
+        assert.equal(null, err);
 
-          dbName.listCollections().toArray((err, items) => {
-              test.equal(null, err);
-              client.close();
-          });
+        dbName.listCollections().toArray((err, items) => {
+          assert.equal(null, err);
+          client.close();
+        });
       });
-      
+
       res.send("Book is added to the database");
+    })
+    .delete((req, res, next) => {
+      coll.remove();
+      assert.equal(null, err);
+      assert.equal(0, items.length);
+      db.close();
     });
+
+  // route to title
+
+  bookRouter.route("/:title")
+  .get((req, res, next) => {
+    coll.find({}).toArray((err, docs) => {
+        if (!err) {
+            for (let i = 0; i < docs.length; i++) {
+              if (docs[i].title == req.params.title) {
+                console.log("We found: " + docs[i].title);
+                res.sendFile(path.join(__dirname + `/${docs[i].title}.html`));
+                // res.end(`<html><body><h1>${docs[i].title}</h1></body></html>`);
+              }
+            }
+          }
+        })
+  });
 });
 
 module.exports = bookRouter;
