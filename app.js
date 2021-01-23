e267ad04bab6ede6f184304db861fbc7a6b30b9a
Gabriel Cornejo <gcornejo441@gmail.com>
2021-01-22 18:26:45 -0800

    Pug to Ejs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const ejs = require('ejs');
 
 const bookRouter = require('./routes/create-a-movie');
 var indexRouter = require('./routes/index');
@@ -12,13 +13,18 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+
+app.get('/sample', (req, res) => {
+  res.render('sample');
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', 'home.html'));
-    
+  // res.sendFile(path.join(__dirname, '../public', 'home.html'));
+
+  ejs.renderFile('pages/sample', function(err, str){
+    // str => Rendered HTML string
+});
 });
 
 module.exports = router;
