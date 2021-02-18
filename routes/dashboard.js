13dc393893b337a0d5f3fe758d1777e4912d6f54
Gabriel Cornejo <gcornejo441@gmail.com>
2021-02-17 18:58:26 -0800

    CRUD addition

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -11,11 +11,13 @@ const Movies = require("../models/movieSchema");
 
 dashRouter.get("/", ensureAuthenticated, (req, res, next) => {
   Movies.find({}, (err, result) => {
+    if (err) new Error(err);
     res.render("dashboard", { result: result });
   }).catch((err) => next(err));
 });
 
-dashRouter.get("/delete/:id", ensureAuthenticated, (req, res, next) => {
+
+dashRouter.post("/delete/:id", ensureAuthenticated, (req, res, next) => {
   Movies.findByIdAndRemove({ _id: req.params.id }, (err, doc) => {
     console.log(doc);
     if (!err) {
@@ -26,14 +28,25 @@ dashRouter.get("/delete/:id", ensureAuthenticated, (req, res, next) => {
   });
 });
 
+// GET /dashboard/edit/_id
+dashRouter.get("/edit/:id", ensureAuthenticated, (req, res, next) => {
+  console.log("Movie Id: ", req.params.id);
+  console.log("Body info: ", req.body);
+  Movies.findById(req.params.id, (err, result) => {
+    if (err) console.log(err);
+    console.log(result);
+    res.render("edit", { result: result });
+  }).catch((err) => next(err));
+});
+
 dashRouter.post("/edit/:id", ensureAuthenticated, (req, res, next) => {
   console.log("Movie Id: ", req.params.id);
   console.log("Body info: ", req.body);
-
   Movies.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
     if (err) throw handleError(err);
+    console.log('Document to be updated: ', doc)
     res.redirect("/dashboard");
-  });
+  }).catch((err) => next(err))
 });
 
 module.exports = dashRouter;
